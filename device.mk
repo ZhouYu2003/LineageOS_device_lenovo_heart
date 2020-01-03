1084a5e5117252cb66257ca2ad4729d016ef0152
Demon Singur <demonsingur@gmail.com>
2020-01-04 03:14:17 +0530

    zippo: Build IPCam

--- device.mk
+++ device.mk
@@ -212,6 +212,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.heart
 
+# IPA
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml
+
 # IRSC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
