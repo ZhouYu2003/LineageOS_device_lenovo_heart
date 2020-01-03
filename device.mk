c9fc5b31c8d838a5d181648cd74eee900495a4b1
Akash <akashniki@gmail.com>
2020-01-04 03:16:26 +0530

    zippo: Build netutils-wrapper

--- device.mk
+++ device.mk
@@ -260,9 +260,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles_vendor.xml:system/etc/media_profiles_vendor.xml
 
-# Net
+# Network
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
+
+PRODUCT_COPY_FILES += \                                                                frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
 # NFC
 PRODUCT_COPY_FILES += \
