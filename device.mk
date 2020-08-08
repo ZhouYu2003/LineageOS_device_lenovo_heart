8cb19fac53557b4a35ed6e4dafa55761a3f1e456
Lucchetto <lucchetto.tie@live.com>
2020-08-08 16:27:56 +0200

    Revert "zippo: build light hal"
    This reverts commit 697f5602c4c295209757564898ddd3b0af685fd4.
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- device.mk
+++ device.mk
@@ -249,11 +249,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
-# Lights
-PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service
-
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
@@ -300,6 +295,7 @@ PRODUCT_PACKAGES += \
 # Slider handler
 PRODUCT_PACKAGES += \
     KeyHandler
+
 
 # OMX
 PRODUCT_PACKAGES += \
