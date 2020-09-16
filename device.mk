ea8efa6413151be508a68768902a50a05b45d7c8
kubersharma001 <kubersharma001@gmail.com>
2020-09-17 00:37:52 +0530

    heart: Build libhwbinder and libhidltransport
    Change-Id: Ie811d4c1560fb4104674d1300319a2f6c3ee2985

--- device.mk
+++ device.mk
@@ -43,6 +43,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/configs/audio/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio/audio_policy_configuration.xml
 
+# Binder
+PRODUCT_PACKAGES += \
+    libhwbinder
+
 # Camera
 PRODUCT_PACKAGES += \
     Snap
@@ -64,7 +68,8 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.manager@1.0 \
+    libhidltransport
 
 # Input
 PRODUCT_COPY_FILES += \
