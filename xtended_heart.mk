7e2e0da4d6bd994c62e3e286fb804c202fff2269
Einar Gednochsson <pete80pro.pb@gmail.com>
2020-11-21 12:15:05 +0100

    heart: Lets Get Official

--- xtended_heart.mk
+++ xtended_heart.mk
@@ -8,6 +8,8 @@ $(call inherit-product, device/lenovo/heart/device.mk)
 
 # Inherit some common Xtended stuff.
 $(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+XTENDED_BUILD_TYPE := OFFICIAL
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Lenovo
