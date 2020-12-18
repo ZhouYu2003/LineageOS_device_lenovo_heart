4b018c62ab75158b177f04bb5a136e8de78f93bb
ZhouYu2003 <2359292349@qq.com>
2020-12-18 21:11:11 +0800

    heart: Use Coral Fingerprint

--- lineage_heart.mk
+++ lineage_heart.mk
@@ -16,7 +16,7 @@ PRODUCT_MANUFACTURER := Lenovo
 PRODUCT_MODEL := Lenovo Z5 Pro GT
 PRODUCT_NAME := lineage_heart
 
-BUILD_FINGERPRINT := "Lenovo/heart/heart:10/QKQ1.191014.001/11.5.247_200424:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:11/RQ1A.201205.008/6943376:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="heart-user 10 QKQ1.191014.001 11.5.247_200424 release-keys" \
